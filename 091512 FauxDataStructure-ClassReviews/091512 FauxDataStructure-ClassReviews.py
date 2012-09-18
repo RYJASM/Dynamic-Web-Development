82d7442711b4409b0ce635f00087a45215668ae1
mark breneman <mark.breneman@gmail.com>
2012-09-18 10:53:38 -0400

    created dictionaries and class list

--- 091512 FauxDataStructure-ClassReviews/091512 FauxDataStructure-ClassReviews.py
+++ 091512 FauxDataStructure-ClassReviews/091512 FauxDataStructure-ClassReviews.py
@@ -1,56 +1,94 @@
 # This is fake data structure for my theoretical class review system
 
-Class={
-	'name':'cadbury',
-	'weight': 3.5,
-	'type':'milk chocolate',
-	'color':'purple'
+Dynamic_Web_Development={
+	'name':'Dynamic Web Development',
+	'professor':'J.Schimmel',
+	'term':'Fall',
+	'year':2012,
+	'capacity':16,
+	'website':'http://itppyweb.herokuapp.com/',
+	'id':00001
 }
 
-Twix={
-	'name':'Twix',
-	'weight': 3.02,
-	'type':'caramel',
-	'color':'gold'
+Understanding_Networks={
+	'name':'Understanding Networks',
+	'professor':'T.igoe',
+	'term':'Fall',
+	'year':2012,
+	'capacity':16,
+	'website':'http://itp.nyu.edu/understandingnetworks/',
+	'id':00002
 }
 
-Butterfinger={
-	'name':'Butterfinger',
-	'weight': 3.7,
-	'type':'peanut butter',
-	'color':'yellow'
+
+Printing_Code={
+	'name':'Printing Code',
+	'professor':'R.Madsen',
+	'term':'Fall',
+	'year':2012,
+	'capacity':16,
+	'website':'http://www.runemadsen.com/printing-code-2012',
+	'id':00003
 }
 
-MandM={
-	'name':'M&M',
-	'weight': 1.08,
-	'type':'peanut',
-	'color':'brown','yellow','blue'
+Visualizing_JS={
+	'name':'Visualizing JS',
+	'professor':'S.Smith',
+	'term':'Fall',
+	'year':2012,
+	'capacity':16,
+	'website':'http://stewd.io/javascript/index.html',
+	'id':00004
 }
 
-Lindt={
-	'name':'Lindt',
-	'weight': 3.5,
-	'type':'dark chocolate',
-	'color':'beige'
+MakeMatics={
+	'name':'MakeMatics',
+	'professor':'G.Borenstein',
+	'term':'Fall',
+	'year':2012,
+	'capacity':16,
+	'website':'http://makematics.com/syllabus/2012-Fall/',
+	'id':00005
 }
+
+
+Review={
+	'coursename':'Dynamic Web Development',
+	'year': 2013,
+	'id':000001,
+	'review_date':'2012-09-15',
+	'review_comment':'This class is amazing, and needs to be offered every semester...',
+	'reviewusefulnessindex':3 
+}
+
+Reviewer={
+	'name':'mark breneman',#this could be optional
+	'course_name':'Dynamic Web Development',
+	'year': 2013,
+	'coursework_link':'www.markbreneman.com/blog',
+	'previous_courses': ['PComp', 'Comm.Lab Web', 'Comm.Lab Video and Sound', 'ICM', 'Comm.Lab 2D-Design','HTML5','Nature of Code', 'Wildlife Tools', 'Designing Conversational Spaces'],
+	'id':0001
+}
+
+
+
 
 #Create a list of dictionaries
-# print "*" *75 
-Candylist=[Cadbury,Twix,Butterfinger,MandM,Lindt]
+print "*" *75 
+Classlist=[Dynamic_Web_Development, Understanding_Networks, Printing_Code, Visualizing_JS, MakeMatics]
 
 #Loop though list and print out data
-# print "*" *75 
-#for candy in Candylist:
-	# print candy.get('name'), 'weighs', candy.get('weight'), 'is of type', candy.get('type'), 'and is in %s' % candy.get('color')
+print "*" *75 
+for classes in Classlist:
+	print classes.get('name'), 'is taught by', classes.get('professor'), 'in the', classes.get('term'),  classes.get('year'), 'and has a website at %s' % classes.get('website')
 
 #Create a class for the objects
 print "*" *75 
-class candyobject(object):
+class classobject(object):
 	
 	# __init__ automatically called when creating an object
 	def __init__(self, data=None): #none is a datatype
-		print "Creating new candybar:", data.get('name')
+		print "Creating new class:", data.get('name')
 		self.name = data.get('name')
 		self.weight = data.get('weight')
 		self.type = data.get('type')
@@ -64,25 +102,25 @@ class candyobject(object):
 	# 			return self.name + "Candybar"
 	
 #create an object using the class
-# candyobject(Lindt)
+# classobject(Lindt)
 
 
 # Create a list of the objects
-candyobjectlist=[]		
-for candy in Candylist:
-	candyobjectlist.append( candyobject(candy) )
-
-#Print the list of candy objects weight
-for candyobject in candyobjectlist: 
-	print "*" *75 
-	print candyobject.name
-	print candyobject.weight
-	print candyobject.type
-	print candyobject.color
-print "*" *75 
-for candyobject in candyobjectlist: 
-	print "*" *75 
-	print candyobject
+# classobjectlist=[]		
+# for class in Classlist:
+# 	classobjectlist.append( classobject(class) )
+# 
+# #Print the list of class objects weight
+# for classobject in classobjectlist: 
+# 	print "*" *75 
+# 	print classobject.name
+# 	print classobject.weight
+# 	print classobject.type
+# 	print classobject.color
+# print "*" *75 
+# for classobject in classobjectlist: 
+# 	print "*" *75 
+# 	print classobject
 
 
 	
