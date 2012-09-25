a7b63cd96e7bfc1e4d295c99963bbecdfb6505d8
mark breneman <mark.breneman@gmail.com>
2012-09-25 12:55:34 -0400

    Updated Basic Web App on Heroku files.

--- 092412_BasicWebApponHeroku/app.py
+++ 092412_BasicWebApponHeroku/app.py
@@ -3,14 +3,7 @@ from flask import Flask, request # Retrieve Flask, our framework
 from flask import render_template
 app = Flask(__name__)   # create our flask app
 
-# # this is our main page
-# @app.route("/")
-# def index():
-#     return """Hello World!<br/><br/>
-#     <a href='/page2'>Visit page #2</a><br>
-#     <a href='/form'>Visit form page</a>"""
-
-# # this is my main page
+# this is my main page
 @app.route("/")
 def index():
     return render_template('main.html')
@@ -42,5 +35,5 @@ def simpleform():
 if __name__ == "__main__":
 	app.debug = True
 	
-	port = int(os.environ.get('PORT', 4777)) # locally PORT 5000, Heroku will assign its own port
+	port = int(os.environ.get('PORT', 5000)) # locally PORT 5000, Heroku will assign its own port
 	app.run(host='0.0.0.0', port=port)
